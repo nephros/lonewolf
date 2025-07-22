4c0d34d3931e3ea38d1e4256d6bf7f4b43acb929
nephros <nemo@JollaC2>
2025-07-22 23:37:00 +0200

    print dl error

--- backend/modules/Lonewolf/downloader.cpp
+++ backend/modules/Lonewolf/downloader.cpp
@@ -67,6 +67,7 @@ void Downloader::fileFinished()
     for (int i = 0; i < m_replies.length(); i++) {
         FileData &data = m_replies[i];
         if (data.reply == sender()) {
+            qDebug() << Q_FUNC_INFO << "error" << data.reply->errorString();
             QFile localFile(m_cacheDir + "/" + data.reply->url().fileName());
             qDebug() << Q_FUNC_INFO << "writing" << data.total << "bytes to" << localFile.fileName();
             if (localFile.open(QIODevice::WriteOnly)) {
